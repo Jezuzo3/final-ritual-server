3dbea3ad68e8683065ab68890fda9244fc9c8966
ANDER ARQUERO CARRASQUEDO <ander.arquero@ikasle.aeg.eus>
2023-02-06 13:30:16 +0100

    Agregadas las constantes

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,33 @@
 const NEW_CONNECTION = "new_connection";
 const UPDATE_ONCRYPT = "update_oncrypt";
 const DISCONNECT = "disconnect";
+const INVOKE_CIRCLE = "invoke_circle";
+const START_RITE = "start_rite";
+const ARCANE_READING = "arcane_reading";
+const VILLAIN_SABOTAGE = "villain_sabotage";
+const LAUNCH_SPECTRAL_DIVISION = "launch_spectral_division";
+const MORTIFYING_ATTACK = "mortifying_attack";
+const FINAL_BATTLE_RESULT = "final_battle_result";
+const ACCEPT_ACOLYTE_RITUAL = "accept_acolyte_ritual";
+const START_ACOLYTE_PRAYER = "start_acolyte_prayer";
+const ACOLYTE_READY = "acolyte_ready";
+const SPECTRAL_DIVISION = "spectral_division";
+const ACOLYTE_PROGRESS = "acolyte_progress";
 
 module.exports = {
     NEW_CONNECTION,
     UPDATE_ONCRYPT,
-    DISCONNECT
+    DISCONNECT,
+    INVOKE_CIRCLE,
+    START_RITE,
+    ARCANE_READING,
+    VILLAIN_SABOTAGE,
+    LAUNCH_SPECTRAL_DIVISION,
+    MORTIFYING_ATTACK,
+    FINAL_BATTLE_RESULT,
+    ACCEPT_ACOLYTE_RITUAL,
+    START_ACOLYTE_PRAYER,
+    ACOLYTE_READY,
+    SPECTRAL_DIVISION,
+    ACOLYTE_PROGRESS
 }
