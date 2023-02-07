d19902e62807f9beb7399be6aface9b0dc1a7610
Josu Etxeberria <josu.echeverria@ikasle.aeg.eus>
2023-02-07 13:38:10 +0100

    [UPDATE] Socket listeners

--- src/constants.js
+++ src/constants.js
@@ -8,27 +8,33 @@ const ARCANE_READING = "arcane_reading";
 const VILLAIN_SABOTAGE = "villain_sabotage";
 const LAUNCH_SPECTRAL_DIVISION = "launch_spectral_division";
 const MORTIFYING_ATTACK = "mortifying_attack";
+const MORTIFYING_BATTLE = "mortifying_battle";
 const FINAL_BATTLE_RESULT = "final_battle_result";
 const ACCEPT_ACOLYTE_RITUAL = "accept_acolyte_ritual";
 const START_ACOLYTE_PRAYER = "start_acolyte_prayer";
 const ACOLYTE_READY = "acolyte_ready";
-const SPECTRAL_DIVISION = "spectral_division";
+const ACOLYTE_BUTTON_SPECTRAL_DIVISION = "acolyte_button_spectral_division";
 const ACOLYTE_PROGRESS = "acolyte_progress";
+const FINAL_SCREEN = "final_screen";
+const RESTART_RITUAL = "restart_ritual";
 
 module.exports = {
-    NEW_CONNECTION,
-    UPDATE_ONCRYPT,
-    DISCONNECT,
-    INVOKE_CIRCLE,
-    START_RITE,
-    ARCANE_READING,
-    VILLAIN_SABOTAGE,
-    LAUNCH_SPECTRAL_DIVISION,
-    MORTIFYING_ATTACK,
-    FINAL_BATTLE_RESULT,
-    ACCEPT_ACOLYTE_RITUAL,
-    START_ACOLYTE_PRAYER,
-    ACOLYTE_READY,
-    SPECTRAL_DIVISION,
-    ACOLYTE_PROGRESS
-}
+  NEW_CONNECTION,
+  UPDATE_ONCRYPT,
+  DISCONNECT,
+  INVOKE_CIRCLE,
+  START_RITE,
+  ARCANE_READING,
+  VILLAIN_SABOTAGE,
+  LAUNCH_SPECTRAL_DIVISION,
+  MORTIFYING_ATTACK,
+  MORTIFYING_BATTLE,
+  FINAL_BATTLE_RESULT,
+  ACCEPT_ACOLYTE_RITUAL,
+  START_ACOLYTE_PRAYER,
+  ACOLYTE_READY,
+  ACOLYTE_BUTTON_SPECTRAL_DIVISION,
+  ACOLYTE_PROGRESS,
+  FINAL_SCREEN,
+  RESTART_RITUAL,
+};
