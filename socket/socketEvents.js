033d5cf836efbff22e47e549caf6eaa42fad2df4
Josu Etxeberria <josu.echeverria@ikasle.aeg.eus>
2023-02-08 11:45:58 +0100

    [UPDATE] listeners and clean code

--- socket/socketEvents.js
+++ socket/socketEvents.js
@@ -22,59 +22,31 @@ events = (socket) => {
     }
   });
 
-  // Listener para actualizar estado onCrypt
-  socket.on(constants.UPDATE_ONCRYPT, async (data) => {
+  // Listener para cambiar el estado de la misión
+  socket.on(constants.CHANGE_MISSION_STATUS, async (data) => {
+    try {
+      io.emit(constants.CHANGE_MISSION_STATUS, data);
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  // Listener para cambiar el estado del usuario
+  socket.on(constants.CHANGE_USER_STATUS, async (data) => {
     try {
       data.body = {
-        userState: "onCrypt",
+        userState: data.userState,
       };
       const updatedUser = await userService.updateUserByEmail(
         data.email,
         data.body
       );
-
-      const idSockets = [socket.id, updatedUser.idSocket];
+      const idSockets = [updatedUser.idSocket];
       const villainIdSocket = await userService.getIdSocketByRol("villain");
       const mortimerIdSocket = await userService.getIdSocketByRol("mortimer");
       idSockets.push(villainIdSocket, mortimerIdSocket);
 
-      io.to(idSockets).emit(constants.UPDATE_ONCRYPT, updatedUser);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para invocar circulo
-  socket.on(constants.INVOKE_CIRCLE, async () => {
-    try {
-      const data = {
-        missionStatus: constants.INVOKE_CIRCLE,
-      };
-      io.emit(constants.INVOKE_CIRCLE, data);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para iniciar el rito
-  socket.on(constants.START_RITE, async () => {
-    try {
-      const data = {
-        missionStatus: constants.START_RITE,
-      };
-      io.emit(constants.START_RITE, data);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para empezar la lectura arcana
-  socket.on(constants.ARCANE_READING, async () => {
-    try {
-      const data = {
-        missionStatus: constants.ARCANE_READING,
-      };
-      io.emit(constants.ARCANE_READING, data);
+      io.to(idSockets).emit(constants.CHANGE_USER_STATUS, updatedUser);
     } catch (error) {
       throw error;
     }
@@ -95,25 +67,13 @@ events = (socket) => {
     }
   });
 
-  // Listener para lanzar división espectral
-  socket.on(constants.LAUNCH_SPECTRAL_DIVISION, async () => {
-    try {
-      const data = {
-        missionStatus: constants.LAUNCH_SPECTRAL_DIVISION,
-      };
-      io.emit(constants.LAUNCH_SPECTRAL_DIVISION, data);
-    } catch (error) {
-      throw error;
-    }
-  });
-
   // Listener para manejar el ataque del villano a mortimer
   socket.on(constants.MORTIFYING_ATTACK, async (data) => {
     try {
       const res = {
         missionStatus: constants.MORTIFYING_ATTACK,
       };
-      io.emit(constants.MORTIFYING_ATTACK, res);
+      io.emit(constants.CHANGE_MISSION_STATUS, res);
 
       let finished = false;
       let counter = 1;
@@ -153,122 +113,11 @@ events = (socket) => {
     }
   });
 
-  // Listener para aceptar ritual
-  socket.on(constants.ACCEPT_ACOLYTE_RITUAL, async (data) => {
-    try {
-      data.body = {
-        userState: "onRitual",
-      };
-      const updatedUser = await userService.updateUserByEmail(
-        data.email,
-        data.body
-      );
-      const idSockets = [updatedUser.idSocket];
-      const villainIdSocket = await userService.getIdSocketByRol("villain");
-      const mortimerIdSocket = await userService.getIdSocketByRol("mortimer");
-      idSockets.push(villainIdSocket, mortimerIdSocket);
-
-      io.to(idSockets).emit(constants.ACCEPT_ACOLYTE_RITUAL, updatedUser);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para inicial plegaria el acólito
-  socket.on(constants.START_ACOLYTE_PRAYER, async (data) => {
-    try {
-      data.body = {
-        userState: "praying",
-      };
-      const updatedUser = await userService.updateUserByEmail(
-        data.email,
-        data.body
-      );
-      const idSockets = [updatedUser.idSocket];
-      const villainIdSocket = await userService.getIdSocketByRol("villain");
-      const mortimerIdSocket = await userService.getIdSocketByRol("mortimer");
-      idSockets.push(villainIdSocket, mortimerIdSocket);
-
-      io.to(idSockets).emit(constants.START_ACOLYTE_PRAYER, updatedUser);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para actualizar el estado del acólito a ready
-  socket.on(constants.ACOLYTE_READY, async (data) => {
-    try {
-      data.body = {
-        userState: "ready",
-      };
-      const updatedUser = await userService.updateUserByEmail(
-        data.email,
-        data.body
-      );
-      const idSockets = [updatedUser.idSocket];
-      const villainIdSocket = await userService.getIdSocketByRol("villain");
-      const mortimerIdSocket = await userService.getIdSocketByRol("mortimer");
-      idSockets.push(villainIdSocket, mortimerIdSocket);
-
-      io.to(idSockets).emit(constants.ACOLYTE_READY, updatedUser);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para que el acólito lanze la división espectral
-  socket.on(constants.ACOLYTE_BUTTON_SPECTRAL_DIVISION, async (data) => {
-    try {
-      data.body = {
-        userState: "launched",
-      };
-      const updatedUser = await userService.updateUserByEmail(
-        data.email,
-        data.body
-      );
-      const idSockets = [updatedUser.idSocket];
-      const villainIdSocket = await userService.getIdSocketByRol("villain");
-      const mortimerIdSocket = await userService.getIdSocketByRol("mortimer");
-      idSockets.push(villainIdSocket, mortimerIdSocket);
-
-      io.to(idSockets).emit(
-        constants.ACOLYTE_BUTTON_SPECTRAL_DIVISION,
-        updatedUser
-      );
-    } catch (error) {
-      throw error;
-    }
-  });
-
   // Listener para enviar el progreso de la barra
   socket.on(constants.ACOLYTE_PROGRESS, async (data) => {
     try {
       const villainIdSocket = await userService.getIdSocketByRol("villain");
       io.to(villainIdSocket).emit(constants.ACOLYTE_PROGRESS, data);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para enseñar la pantalla final
-  socket.on(constants.FINAL_SCREEN, async () => {
-    try {
-      const data = {
-        missionStatus: constants.FINAL_SCREEN,
-      };
-      io.emit(constants.FINAL_SCREEN, data);
-    } catch (error) {
-      throw error;
-    }
-  });
-
-  // Listener para reiniciar la aventura
-  socket.on(constants.RESTART_RITUAL, async () => {
-    try {
-      const data = {
-        missionStatus: null,
-      };
-      io.emit(constants.RESTART_RITUAL, data);
     } catch (error) {
       throw error;
     }
